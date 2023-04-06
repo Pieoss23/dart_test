c5b45f326e456b34089fd10d87739cfb04a04ee4
Pietro Oss <poss@MacBook-Pro-di-Alessandro.local>
2023-04-06 17:53:36 +0200

    first commit

---
+++ lib/model/user_location.dart
@@ -0,0 +1,50 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class UserLocation {
+  final String city;
+  final String state;
+  final String country;
+  final String postcode;
+  final LocationStreet street;
+  final LocationCoordinate coordinates;
+  final LocationTimezone timezone;
+
+  UserLocation({
+    required this.city,
+    required this.state,
+    required this.country,
+    required this.postcode,
+    required this.coordinates,
+    required this.street,
+    required this.timezone,
+  });
+}
+
+class LocationStreet {
+  final int number;
+  final String name;
+  LocationStreet({
+    required this.name,
+    required this.number,
+  });
+}
+
+class LocationCoordinate {
+  final String latitude;
+  final String longitude;
+  LocationCoordinate({
+    required this.latitude,
+    required this.longitude,
+  });
+}
+
+class LocationTimezone {
+  final String offset;
+  final String description;
+  LocationTimezone({
+    required this.description,
+    required this.offset,
+  });
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,48 @@
+import 'package:rest_api/model/user.dart';
+import 'package:flutter/material.dart';
+import 'package:rest_api/services/user_api.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<User> users = [];
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('USERS'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          final user = users[index];
+          // final color = user.gender == 'male' ? Colors.blue : Colors.pinkAccent;
+          return ListTile(
+            title: Text(user.fullName),
+            subtitle: Text(
+                '${user.location.city} - ${user.location.country} - ${user.location.state}'),
+            // tileColor: color,
+          );
+        },
+      ),
+    );
+  }
+
+  Future<void> fetchUsers() async {
+    final response = await UserApi.fetchUsers();
+    setState(() {
+      users = response;
+    });
+  }
+}
